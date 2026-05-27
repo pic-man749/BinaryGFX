487e5474f8625952013ecd591b0227ab70cc5b21
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2026-05-28 00:25:09 +0900

    デュアルバッファ化

--- Core/Src/BinaryGFXCore.cpp
+++ Core/Src/BinaryGFXCore.cpp
@@ -10,7 +10,13 @@
 namespace BinaryGFX {
 
   BinaryGFX::BinaryGFX(std::unique_ptr<Driver::IDisplayDriver> driver) :
-      m_driver(std::move(driver)), m_frameBuffer(m_driver->getWidth(), m_driver->getHeight()), m_nextId(1u) {
+      m_driver(std::move(driver)),
+      m_frameBuffers{
+          FrameBuffer(m_driver->getWidth(), m_driver->getHeight()),
+          FrameBuffer(m_driver->getWidth(), m_driver->getHeight())
+      },
+      m_activeBuffer(1u),
+      m_nextId(1u) {
   }
 
   bool BinaryGFX::init() {
@@ -31,12 +37,18 @@ namespace BinaryGFX {
   }
 
   ErrorCode BinaryGFX::update() {
-    m_frameBuffer.clear();
-    renderAll();
-    return m_driver->sendBuffer(m_frameBuffer.getData(), m_frameBuffer.getDataSize());
+    // 前回と別のバッファへレンダリングする
+    const uint8_t renderIdx = m_activeBuffer ^ 1u;
+    m_frameBuffers[renderIdx].clear();
+    renderAll(m_frameBuffers[renderIdx]);
+
+    // レンダリング済みバッファを切り替える
+    // DMA使用時: sendBuffer()内で前フレームDMA完了を待ってから転送開始する
+    m_activeBuffer = renderIdx;
+    return m_driver->sendBuffer(m_frameBuffers[m_activeBuffer].getData(), m_frameBuffers[m_activeBuffer].getDataSize());
   }
 
-  void BinaryGFX::renderAll() {
+  void BinaryGFX::renderAll(FrameBuffer &fb) {
     std::sort(m_objects.begin(), m_objects.end(), [](const ObjectEntry &a, const ObjectEntry &b) {
       if(a.object->getZ() != b.object->getZ()) {
         return a.object->getZ() < b.object->getZ();
@@ -44,7 +56,7 @@ namespace BinaryGFX {
       return a.id < b.id;
     });
     for(const auto &entry : m_objects) {
-      entry.object->render(m_frameBuffer);
+      entry.object->render(fb);
     }
   }
 
